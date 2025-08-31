83195c166b8afab5db1e4e3a4ef3e54710a55fc6
Affan-30 <affansshaikh76@gmail.com>
2025-08-31 12:57:53 +0530

    Updated React app

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
     <div className="app">
       <div className="header-section">
         <div className="search-container">
-          <img src="./src/assets/logo.png" alt="" className="logo"/>
+          <img src="./assets/logo.png" alt="CloudCast" className="logo"/>
           
           <div className="input-wrapper">
             <input
